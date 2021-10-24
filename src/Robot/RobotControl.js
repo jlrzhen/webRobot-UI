2abb07d6063e9ed242acae1fb14e07735105f437
Jeffrey Zhen <jeffreyleran.zhen@uwaterloo.ca>
2021-10-24 09:38:51 -0400

    Handle fetch responses

--- src/Robot/RobotControl.js
+++ src/Robot/RobotControl.js
@@ -1,5 +1,14 @@
 export default function sendCoords(stickX, stickY, mouseDown, url) {
     console.log(`${stickX} ${stickY} ${mouseDown}`);
     // fetch(`${process.env.REACT_APP_FLASK_URL}${stickX}/${stickY}`);
-    fetch(`${url}${stickX}/${stickY}`);
+    fetch(`${url}${stickX}/${stickY}`).then((response) => {
+    if (response.ok) {
+        return response.json();
+    } else {
+        throw new Error('Something went wrong');
+    }
+    })
+    .catch((error) => {
+    //console.log(error)
+    });
 }
